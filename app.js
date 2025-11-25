f853df7b4d4f9f7a87aa3b1014643bf3ab62e94b
brian114 <brian.sneider.n2@gmail.com>
2025-11-24 22:38:14 -0500

    Agregar módulo footer

---
+++ app.js
@@ -0,0 +1,6 @@
+/* --- Footer del sistema (feature/footer) --- */
+function cargarFooter() {
+  console.log("Cargando footer del sistema...");
+}
+
+console.log("Módulo footer agregado desde feature/footer.");
